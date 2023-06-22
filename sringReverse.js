f90d196ab9882717e8609b46748b9076b9d50d46
Himanshu <himanshu6657@gmail.com>
2023-06-22 23:02:24 +0530

    string reverse program

---
+++ sringReverse.js
@@ -0,0 +1,10 @@
+const string ='hello';
+//here we just split the string to make thi into array
+let arr = string.split("");
+
+
+//now we make an reverse loop for descending
+for (let i =arr.length-1; i >0; i--) {
+    console.log(arr[i],'donnneeee')
+    
+}
