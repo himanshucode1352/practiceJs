7a2301b13279277733dbe77ce5e229e39347eb99
Himanshu <himanshu6657@gmail.com>
2023-06-30 11:44:32 +0530

    another method to do this

--- duplicateValue.js
+++ duplicateValue.js
@@ -13,7 +13,7 @@ var arr =[1,2,3,4,1,3,2,4,6,7,8,9]
 
 for(let i=0;i<arr.length;i++){
     for (let j =i+1;j<arr.length;j++){
-        console.log(i,'-------',j)//this loop will add + value to the loop and compares till end 
+        console.log(i,'-------',j)//this loop will add + value to the loop and compares till end and then outer loop will change then it still goes onn
         if(arr[i]===arr[j]){
 console.log(arr[i])
         }
