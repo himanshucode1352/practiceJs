e1aefaff3359b5a015064c1e923c47bd7fc49501
himanshu <himtri6657@gmail.com>
2023-12-03 01:01:27 +0530

    starPattern

--- starPattern.js
+++ starPattern.js
@@ -10,8 +10,14 @@ console.log(output)
 
 const printStar=(n)=>{
     for (i=0;i<=n;i++){  // this function will send calue or initialize above function
-        starLine(i)
+       // starLine(i)
+       var result=''
+       for (j=0;j<i;j++){
+        result= result+'*'
+       }
+       console.log(result)   
     }
+   
 }
 
 printStar(3) //hence proved
